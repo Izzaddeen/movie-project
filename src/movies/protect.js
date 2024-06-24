83e72a45f59555532023a8d7d14ec025f2a3fa06
Izzaddeen <izzarex05@gmail.com>
2024-06-24 12:39:40 +0530

    html  changes

--- src/movies/protect.js
+++ src/movies/protect.js
@@ -1,39 +1,3 @@
-// import React from "react";
-// import { Navigate, Outlet } from "react-router-dom";
-
-
-// const Protect = ({ isLoggedIn }) => {
-    
-//       if (!isLoggedIn) {
-//         return <Navigate to="/" />; 
-//       }
-    
-//     return <Outlet /> ;
-//   };
-// export default Protect;
-
-
-
-
-
-
-// import React from "react";
-// import { Navigate, Outlet } from "react-router-dom";
-
-// const Protect = ({ isLoggedIn }) => {
-//   if (!isLoggedIn) {
-//     return <Navigate to="/login" />; 
-//   }
-//   return <Outlet />;
-// };
-
-// export default Protect;
-
-
-
-
-
-
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
